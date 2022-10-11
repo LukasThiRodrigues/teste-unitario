a25fe93f58e4fc973331eedc388f76bac99d96ad
LukasThiRodrigues <lukasthi7@gmail.com>
2022-10-11 10:44:31 -0300

    update

--- src/app/shared/banking/banking.component.spec.ts
+++ src/app/shared/banking/banking.component.spec.ts
@@ -38,10 +38,27 @@ describe('BankingComponent', () => {
     expect(component.getCarteira).toEqual(60);
   });
 
+  it(`(U) setSacar(): shoud transfer poupanca dont have string (isNaN) or poupanca < value`, ()=> {
+    expect(component.setSacar('string')).not.toBeTruthy();
+    expect(component.setSacar('100')).not.toBeTruthy();
+
+    expect(component.getPoupanca).toEqual(10);
+    expect(component.getCarteira).toEqual(50);
+  });
+
   it('(U) setDepositar(): shoud transfer carteira from poupanca', ()=> {
     component.setDepositar('50');
     fixture.detectChanges
     expect(component.getCarteira).toEqual(0);
     expect(component.getPoupanca).toEqual(60);
-  })
+  });
+
+  it(`(U) setDepositar(): shoud transfer carteira dont have string (isNaN) or carteira < value`, ()=> {
+    expect(component.setDepositar('string')).not.toBeTruthy();
+    expect(component.setDepositar('100')).not.toBeTruthy();
+    
+    expect(component.getPoupanca).toEqual(10);
+    expect(component.getCarteira).toEqual(50);
+  });
+
 });
