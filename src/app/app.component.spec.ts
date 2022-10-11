010fb3f4df1c2ac6bc620e44f8d89f784fc84c53
LukasThiRodrigues <lukasthi7@gmail.com>
2022-10-11 09:06:43 -0300

    nova aula

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('teste-unitario app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('tteste-unitario app is running!');
   });
 });
